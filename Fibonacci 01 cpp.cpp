79ceaca42917e802e76e263674aa26abcfa71871
Cris-19 <81449781+Cris-19@users.noreply.github.com>
2021-07-05 16:35:37 -0300

    Add files via upload

---
+++ Fibonacci 01 cpp.cpp
@@ -0,0 +1,31 @@
+   #include <iostream>
+   #include <vector>
+   #include<bits/stdc++.h>
+   using namespace std;
+
+
+
+//fibonacci usando recursão
+//mostra todos os membros da sequencia
+
+  int main() {
+  	
+  int fibonacci;
+  int fib;
+  int i = 0 ; 
+  int n = 34;
+  int f[n + 1];
+  f[0] = 0;
+  f[1] = 1;
+  f[i] = f[i - 1] + f[i - 1];
+
+  for(i = 1; i <= n; i++);
+
+  cout << "n 34 ";
+  cout << "1,2,3,5,10,20";
+  cout<<" 0";
+  cout<<" 10";
+
+
+		return 0;
+}
