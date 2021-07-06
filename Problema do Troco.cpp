94a1b260ef73a6deb1583ef4e8d656979231f766
Cris-19 <81449781+Cris-19@users.noreply.github.com>
2021-07-05 22:32:44 -0300

    Add files via upload

---
+++ Problema do Troco.cpp
@@ -0,0 +1,30 @@
+   #include <iostream>
+   #include <iostream>
+   # include <vector>
+
+   using namespace std;
+   int main() {
+   int main(int argc,char);
+   int num_moedas;  
+   int troco (int valor,int moedas[],int m);
+   int qte_moedas = 0 ;
+   int total = 0;
+   int n =(1,2,3,5,10,20);
+   n = 20;
+  
+  cout<<"int n =(1,2,3,5,10,20)";
+  cout<<" 0";
+  cout<<" 10";
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+  return 0;
+  
+}
+
