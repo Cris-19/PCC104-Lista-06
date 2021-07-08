bb0a406735d9a0a688ff46b9ac1d94ea2a64acc1
Cris-19 <81449781+Cris-19@users.noreply.github.com>
2021-07-08 20:43:17 -0300

    Add files via upload

---
+++ Fibonacci pd.cpp
@@ -0,0 +1,18 @@
+     #include <iostream>
+  #include <iostream>
+  using namespace std;
+  int main() {
+  int fib_progrdinam_bottom_up(int n);
+   int fibonacci(int i );
+   int fibonacci_pd (int n);
+   int fib(int x);
+   int x , i=0;
+   long int fibo(int N);
+   long int a=0,b=1,c;
+
+   cout << "int fibonacci_pd (int n)";
+   cout << "n Fibonnaci Series : ";
+        
+   
+   return 0;
+}
