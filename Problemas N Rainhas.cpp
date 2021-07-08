df83f267796d8e07b5e31bbc4afc6f8f91e7ecab
Cris-19 <81449781+Cris-19@users.noreply.github.com>
2021-07-07 23:04:36 -0300

    Add files via upload

---
+++ Problemas N Rainhas.cpp
@@ -0,0 +1,42 @@
+   #include <iostream>
+   #include <vector>
+   #include <cmath>
+
+
+   using namespace std;
+   int main() {
+   	
+   int n = 0;
+   int sol = 0 ;		
+   void mostrarTabuleiro(vector<vector<int> > & tab, int N);			
+}
+   bool seguro(vector<vector<int> > & tab, int N, int lin, int col)
+{
+	int i, j;
+	for(i = 0; i < N; i++);
+	if(tab[lin][i] == 1);
+	
+	if(seguro(tab, N, i, col));
+	tab[i][col] = 0;
+   	tab[i][col] = 1;
+   	for(i = lin, j = col; i < N && j < N; i++, j++);
+	if(tab[i][j] == 1)
+	if(tab, N, i, col);
+	
+   	int main(int argc, char *argv[]);
+    int n = 8;
+   	vector<int> linha(N);
+   	void executar(vector<vector<int> > & tab, int N, int col);
+	if(col == N);
+	
+	 cout << " Q ";
+     cout << "]";
+	 cout << "R\tab";
+	 cout << "-\t" ;
+	 cout << "\0\1" ;
+     cout << "\n" ;
+    	
+    	
+			return 0;
+}
+
