8f11d30bb8829a78b639bbe14cd5bc34e82f6c23
Cris-19 <81449781+Cris-19@users.noreply.github.com>
2021-07-10 15:44:20 -0300

    Add files via upload

---
+++ Grafo.cpp
@@ -0,0 +1,25 @@
+     #include <iostream>
+   #include <iostream>
+   # include <vector>
+   #include <bits/stdc++.h>
+   using namespace std;
+   class Grafo;
+  
+  int main(){ 
+  int V;
+  int grafo;
+  int color i = j;
+  void adicionararesta(int v1,int v2); 
+  void printGrafo(Grafo* grafo);
+  void printGrafo(vector<int> adj[], int V);
+  void printSolution(int color[],int n);
+  cout << "As cores atribuidas sao:"<<"\n";
+  for (int j = 1; j <= m; j++) {
+  for (int i = 0; i < n; i++);
+  cout <<" "<< color[i] <<" "<< "\n"; 
+  for(int v = 0; v < V; ++v);
+
+
+    return 0 ;
+
+}
