654ef2f26f61993a5fb1ff7ae64fcb9537eeab7d
Cris-19 <81449781+Cris-19@users.noreply.github.com>
2021-07-13 21:47:15 -0300

    Add files via upload

---
+++ Ciclo Hamiltoniano.cpp
@@ -0,0 +1,27 @@
+   #include <iostream>
+   #include <iostream>
+   # include <vector>
+   #include <bits/stdc++.h>
+   using namespace std;
+  
+  int main() 
+{
+  int V;
+  int grafo;
+  int color;
+  int adicionararesta(int v1,int v2); 
+ 
+int Color(int vertices);
+
+ int vertices = 2;
+ int result = 0;
+ if (vertices % 2 == 0);
+ 
+    cout << "color vertices ";
+    cout << " 2 ";
+ 
+
+
+
+  return 0;
+}
